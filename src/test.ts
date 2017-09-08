597c5f28f2b455578ad756a10baf538905604460
Andrew Fong <id@andrewfong.com>
2017-09-07 23:11:40 -0700

    Improved sinon integration

--- src/test.ts
+++ src/test.ts
@@ -1,28 +1,73 @@
 /*
-  Convenience wrapper around tape that sets up Sinon sandboxing.
-  Handles call test.end for us.
+  Convenience wrappers around tape that sets up Sinon sandboxing. Integrates
+  some assertions between sinon and tape. Handles calling test.end. Usage:
+
+    describe('myFunction', it => {
+      it('stores API result', async (assert, sandbox) => {
+        let apiSpy = sandbox.stub(Api, 'getThing', 'id')
+          .returns(Promise.resolve('result'));
+        let storeSpy = sandbox.stub(Store, 'set');
+        await myFunction();
+        assert.calledWith(apiSpy, 'id');
+        assert.calledWith(storeSpy, 'result');
+      });
+    });
 */
 
-import test = require('tape');
+import tape = require('tape');
 import { Test } from 'tape'; // Type
 import * as sinon from 'sinon';
 
-export type TestCase =
-  (assert: Test, sandbox: sinon.SinonSandbox) => void|Promise<void>;
+// Get type with actual Sinon asseriotns
+const { pass, fail, ...sinonAssertTmp } = sinon.assert;
+export const sinonAssert = sinonAssertTmp;
+export type SinonAssertions = typeof sinonAssert;
 
-export default function(name: string, tc: TestCase) {
-  test(name, t => {
-    const sandbox = sinon.sandbox.create();
-    Promise.resolve(tc(t, sandbox)).then(
-      () => {
-        sandbox.restore();
-        t.end();
-      },
-
-      (err) => {
-        sandbox.restore();
-        console.error(err); // Get proper stack trace
-        t.error(err);
-      });
-  });
+// Combine Tape and Sinon assertions / matches into one object.
+export interface Assertions extends Test, SinonAssertions {
+  match: typeof sinon.match;
 }
+
+export type TestCase =
+  (assert: Assertions, sandbox: sinon.SinonSandbox) => void|Promise<void>;
+
+export const test = (name: string, tc: TestCase) => tape(name, t => {
+  const assert: Assertions = {
+    ...t,
+    ...sinon.assert,
+    match: sinon.match
+  };
+
+  // Connect Sinon hooks with Tape
+  sinon.assert.pass = (assertion) => t.pass(assertion);
+  sinon.assert.fail = (message) => {
+    let err = new Error(message);
+    err.name = sinon.assert.failException;
+    t.error(err);
+  };
+
+  // Create sinon sandbox -- restore and call appropriate Tape assertion
+  // at the end of test.
+  const sandbox = sinon.sandbox.create();
+  Promise.resolve(tc(assert, sandbox)).then(
+    () => {
+      sandbox.restore();
+      t.end();
+    },
+
+    (err) => {
+      sandbox.restore();
+      console.error(err); // Get proper stack trace
+      t.error(err);
+    });
+});
+
+// Group tape tests together
+export const describe = function(
+  groupName: string,
+  fn: (it: typeof test) => void
+) {
+  fn((name, tc) => test(`${groupName}: ${name}`, tc));
+};
+
+export default test;
